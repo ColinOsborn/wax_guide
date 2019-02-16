84aa69c7e0fc8788099da70f85adae08ea04995e
Colin Osborn <colin.osborn@gmail.com>
2019-02-16 00:35:14 -0700

    added zipcode api call

--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -21,19 +21,40 @@ describe WeatherService do
         end
     end
 
-    # context '#city_search' do
-    #     it 'returns information for an array of cities that match the search text' do
-    #         VCR.use_cassette("get_city_search", :record => :once) do
-    #         search = WeatherService.new.get_city_search("Aspen")
+    context '#get_postal_code_search' do
+        it 'returns information for an array of postal codes that match the search text' do
+            VCR.use_cassette("get_postal_code_search") do
+            search = WeatherService.new.get_postal_code_search(81611)
 
-    #         expect(search.first["Key"]).to eq(332154)
-    #         expect(search.first["Type"]).to eq("City")
-    #         expect(search.first["LocalizedName"]).to eq("Aspen")
-    #         expect(search.first["EnglishName"]).to eq("Aspen")
-    #         expect(search.first["PrimaryPostalCode"]).to eq(81611)
-    #         end
-    #     end
-    # end
+            expect(search.first["LocalizedName"]).to eq("Aspen")
+            expect(search.first["PrimaryPostalCode"]).to eq("81611")
+            expect(search.first["PrimaryPostalCode"]).to eq("81611")
+            expect(search.first["Region"]["ID"]).to eq("NAM")
+            expect(search.first["Region"]["LocalizedName"]).to eq("North America")
+            expect(search.first["Region"]["EnglishName"]).to eq("North America")
+            expect(search.first["AdministrativeArea"]["ID"]).to eq("CO")
+            expect(search.first["AdministrativeArea"]["LocalizedName"]).to eq("Colorado")
+            expect(search.first["AdministrativeArea"]["Level"]).to eq(1)
+            expect(search.first["AdministrativeArea"]["LocalizedType"]).to eq("State")
+            expect(search.first["TimeZone"]["Code"]).to eq("MST")
+            expect(search.first["TimeZone"]["Name"]).to eq("America/Denver")
+            expect(search.first["TimeZone"]["GmtOffset"]).to eq(-7.0)
+            expect(search.first["TimeZone"]["IsDaylightSaving"]).to eq(false)
+            expect(search.first["TimeZone"]["NextOffsetChange"]).to eq("2019-03-10T09:00:00Z")
+            expect(search.first["GeoPosition"]["Latitude"]).to eq(39.172)
+            expect(search.first["GeoPosition"]["Longitude"]).to eq(-106.725)
+            expect(search.first["GeoPosition"]["Elevation"]["Metric"]["Value"]).to eq(3308.0)
+            expect(search.first["GeoPosition"]["Elevation"]["Metric"]["Unit"]).to eq("m")
+            expect(search.first["GeoPosition"]["Elevation"]["Metric"]["UnitType"]).to eq(5)
+            expect(search.first["GeoPosition"]["Elevation"]["Imperial"]["Value"]).to eq(10852.0)
+            expect(search.first["GeoPosition"]["Elevation"]["Imperial"]["Unit"]).to eq("ft")
+            expect(search.first["GeoPosition"]["Elevation"]["Imperial"]["UnitType"]).to eq(0)
+            expect(search.first["ParentCity"]["Key"]).to eq("332154")
+            expect(search.first["ParentCity"]["LocalizedName"]).to eq("Aspen")
+            expect(search.first["ParentCity"]["EnglishName"]).to eq("Aspen")
+            end
+        end
+    end
 
     context '#get_single_day_forecast' do
         it 'returns daily forecast data for a specific location' do 
