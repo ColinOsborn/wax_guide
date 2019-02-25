656f6d6737c367602267d274f06fc8001f88ef70
Colin Osborn <colin.osborn@gmail.com>
2019-02-24 23:33:10 -0700

    potenitally openstruct

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,13 +1,17 @@
-class Location < OpenStruct
+class Location
     attr_reader :service
 
     def self.service
-        @service ||= WeatherService.new
+        @@service ||= WeatherService.new
     end
 
+    # def self.find(zipcode)
+    #     location_hash = service.get_postal_code_search(zipcode)
+    #     Location.new(location_hash)
+    # end
+
     def self.find(zipcode)
-        location_hash = service.get_postal_code_search(zipcode)
-        Location.new(location_hash)
+        Location.new(service.get_postal_code_search(zipcode))
     end
 
 end
