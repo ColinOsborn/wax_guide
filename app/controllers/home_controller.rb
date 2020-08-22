db98ab09fbfe03207ceec381241058e98dc2bf62
Colin Osborn <colin.osborn@gmail.com>
2020-08-22 17:40:19 -0600

    WIP Search refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,12 @@ class HomeController < ApplicationController
 
     def zipcode 
         @zip_query = params[:zipcode]
-        @output = Weather.service.get_postal_code_search(@zip_query)
+        # @output = Weather.service.get_postal_code_search(@zip_query)
+        @output = Weather.service.get_autocomplete_search(@zip_query)
     end
 
     def location
-        @location_query = params[:location]
+        @location_query = params[:location].pop
         @output = Weather.service.get_current_conditions(@location_query)
     end
 
