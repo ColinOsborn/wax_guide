156a9e73a40f9f74def592ad162db3875b82acb5
Colin Osborn <colin.osborn@gmail.com>
2020-10-08 16:11:13 -0600

    implemeted user creation and authentication setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :home, only: [:index]
   resources :tips, only: [:index]
+  resources :users
+  resources :user_sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'user_sessions#new'
+  get 'logout' => 'user_sessions#destroy'
 
   post "zipcode" => "home#zipcode"
   post "location" => "home#location"
