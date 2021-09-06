d749c41e00f785e5b2614c8fb429f3149d4ebc60
colin <cosborn@patientslikeme.com>
2021-09-06 15:07:04 -0600

    removed old tables before rebuild

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_172539) do
+ActiveRecord::Schema.define(version: 2021_09_06_210320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "skis", force: :cascade do |t|
-    t.string "brand"
-    t.string "model"
-    t.boolean "skate"
-    t.boolean "classic"
-    t.integer "size"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id"
-    t.string "grind"
-    t.index ["user_id"], name: "index_skis_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "email", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "crypted_password", null: false
-    t.string "salt", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
-  add_foreign_key "skis", "users"
 end
