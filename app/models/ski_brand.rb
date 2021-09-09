48571a54aa3665df981f16ad566f40b2282d5388
colin <cosborn@patientslikeme.com>
2021-09-09 10:28:18 -0600

    implemented placeholder module and validations to ski brand

--- app/models/ski_brand.rb
+++ app/models/ski_brand.rb
@@ -1,2 +1,9 @@
 class SkiBrand < ApplicationRecord
+  validates_presence_of :name, :subtitle, :body, :main_image, :thumb_image
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
+    self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
+  end
 end
